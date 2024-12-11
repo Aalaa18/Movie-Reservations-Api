07349b791d8dd77976ccb884e5618b3f72deff44
Aalaa18 <lili.adel.9980@gmail.com>
2024-12-11 10:17:14 +0200

    Add Authorization

--- Controllers/ReservationsController.cs
+++ Controllers/ReservationsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MovieApi.DTO;
@@ -8,6 +9,7 @@ namespace MovieApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles ="ordinary")]
     public class ReservationsController : ControllerBase
     {
         private readonly Ireservationsservices _reservations;
